fb82c60d812b60dd9934afc35c345efac6b0dec0
Student <naimbiswas@yahoo.com>
2024-07-29 16:39:31 +0200

    Update User Interface

--- speech_translation.py
+++ speech_translation.py
@@ -4,6 +4,26 @@ from gtts import gTTS
 import streamlit as st
 import speech_recognition as sr
 from googletrans import LANGUAGES, Translator
+from streamlit_navigation_bar import st_navbar
+
+
+# Add a navigation bar to the app
+
+styles = {
+    "nav": {
+        "background-color": "#655194",
+        "align-items": "left",
+    },
+    "active": {
+        "color": "white",
+        "font-weight": "Bold",
+        "font-size": "20px",
+    }
+     
+}
+
+page = st_navbar(["Real-time Speech Translation Application"], styles=styles)
+
 
 # Global variable to control the translation loop
 isTranslateOn = False
@@ -14,6 +34,27 @@ pygame.mixer.init()
 
 # Create a mapping between language names and language codes    
 language_mapping = {name.capitalize(): code for code, name in LANGUAGES.items()}
+
+#Main content of the app
+st.markdown(
+    """
+    <h1 style='text-align: center; color:#655194; padding-bottom: 30px;'>
+        Real-time Speech Translation
+    </h1>
+    """, 
+    unsafe_allow_html=True
+)
+
+# Get dynamic values from the webpage
+# Page layout
+col1, col2 = st.columns([0.3, 0.7])
+
+with col1:
+    with st.expander("**Settings**", expanded=True):
+        pause_threshold = st.slider("Pause Threshold", min_value=0.5, max_value=3.0, value=1.0, step=0.1)
+        phrase_time_limit = st.number_input("Phrase Time Limit (seconds)", min_value=1, max_value=60, value=10, step=1)
+
+
 
 def get_language_code(language_name):
     """Get the language code from the language name."""
@@ -34,74 +75,76 @@ def text_to_speech(text, language_code):
 
 def listen_and_translate(from_language, to_language):
     """Listen to audio, translate it, and play the translation."""
+   
     rec = sr.Recognizer()
+    status_placeholder = st.empty()  # Placeholder for status updates
+
     with sr.Microphone() as source:
-        
-        with st.spinner("**Listening...**"):
-            rec.pause_threshold = 1
-            audio = rec.listen(source, phrase_time_limit=10)
+        with st.spinner("Listening..."):
+            rec.pause_threshold = pause_threshold
+            audio = rec.listen(source, phrase_time_limit=phrase_time_limit)
     try:
-        st.text("Processing...")
+        status_placeholder.text("Processing...")
         progress_bar = st.progress(0)
         
         spoken_text = rec.recognize_google(audio, language=from_language)
         progress_bar.progress(50)  # Update progress bar to 50%
         
-        st.text("Translating...")
+        status_placeholder.text("Translating...")
         translated_text = translate_text(spoken_text, from_language, to_language)
         progress_bar.progress(100)  # Update progress bar to 100%
         
         text_to_speech(translated_text, to_language)
+        status_placeholder.empty() 
+        progress_bar.empty() # Clear the status placeholder after completion
     
+    except sr.UnknownValueError:
+        status_placeholder.markdown("<span style='color:red'>Sorry, I could not understand the audio. Please speak clear and loudly</span>", unsafe_allow_html=True)
+        progress_bar.empty()
+    except sr.RequestError as e:
+        status_placeholder.markdown(f"<span style='color:red'>Error with the speech recognition service: {e}</span>", unsafe_allow_html=True)
     except Exception as e:
-        st.text(f"Error: {e}")
-
-# Streamlit UI setup
-#st.title("Real-time Speech Application")
-st.markdown(
-    """
-    <h1 style='text-align: center;'>
-    Real-time Speech Application
-    </h1>
-    """, 
-    unsafe_allow_html=True
-)
-
+        status_placeholder.markdown(f"<span style='color:red'>Error during processing or translation: {e}</span>", unsafe_allow_html=True)
+   
 # Dropdown menus for language selection
 capitalized_languages = {code: name.capitalize() for code, name in LANGUAGES.items()}
-from_language_name = st.selectbox("Select Source Language:", list(capitalized_languages.values()))
-to_language_name = st.selectbox("Select Target Language:", list(capitalized_languages.values()))
+with col2:
+    with st.expander("**Select Language**", expanded=True):
+        from_language_name = st.selectbox("Select Source Language:", list(capitalized_languages.values()))
+        to_language_name = st.selectbox("Select Target Language:", list(capitalized_languages.values()))
+
+        # Buttons to start and stop the translation process
+        st.markdown("""
+            <style>
+            .button-container {
+                display: flex;
+                justify-content: space-between;
+            }
+            .stButton button {
+                background-color: #655194; /* Green */
+                color: white;
+                padding: 10px 21px;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 4px 70px;
+                cursor: pointer;
+                border-radius: 4px;
+            }
+            </style>
+            """, unsafe_allow_html=True)
+
+        col1, col2 = st.columns(2)
+        with col1:
+            start_button = st.button("Start")
+        with col2:
+            stop_button = st.button("Stop")
+
 
 # Convert language names to codes
 from_language = get_language_code(from_language_name)
 to_language = get_language_code(to_language_name)
 
-# Buttons to start and stop the translation process
-st.markdown("""
-    <style>
-    .button-container {
-        display: flex;
-        justify-content: space-between;
-    }
-    .stButton button {
-        background-color: #4CAF50; /* Green */
-        color: white;
-        padding: 10px 24px;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: 4px 70px;
-        cursor: pointer;
-        border-radius: 4px;
-    }
-    </style>
-    """, unsafe_allow_html=True)
-
-col1, spacer, col2 = st.columns([1, 1, 1])
-with col1:
-    start_button = st.button("Start")
-with col2:
-    stop_button = st.button("Stop")
 
 # Start translation process when "Start" button is clicked
 if start_button:
